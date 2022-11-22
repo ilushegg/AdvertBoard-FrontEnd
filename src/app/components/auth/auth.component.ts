4c864a45df41e8fac05fe33569804574b44769c8
ilushegg <marnopolskiyi@mail.ru>
2022-11-22 22:03:33 +0300

    Форма регистрации и входа - полность работает, обработчик ошибок - наполовину, карточка товара - наполовину

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, EMPTY } from 'rxjs';
 import { LoginUser } from 'src/app/models/login-user.model';
 import { RegistrationUser } from 'src/app/models/registration-user.model';
 import { AuthService } from 'src/app/services/auth.service';
+import { LoadingService } from 'src/app/services/loading.service';
 
 @Component({
   selector: 'app-login',
@@ -19,13 +20,13 @@ export class AuthComponent implements OnInit {
 
   readonly loginForm = this.formBuilder.group(
     {
-      login: ['', [Validators.required]],
+      email: ['', [Validators.email]],
       password: ['', [Validators.required]]
     }
   );
   readonly registrationForm = this.formBuilder.group(
     {
-      login: ['', [Validators.required]],
+      name: ['', [Validators.required]],
       email: ['', [Validators.email]],
       password: ['', [Validators.required]],
       passwordCheck: ['', [Validators.required]]
@@ -33,10 +34,10 @@ export class AuthComponent implements OnInit {
   );
 
 
-  constructor(private formBuilder: FormBuilder, private nzNotificationService: NzNotificationService, private authService: AuthService, private router: Router ) { }
+  constructor(private formBuilder: FormBuilder, private nzNotificationService: NzNotificationService, private authService: AuthService, private router: Router, public loadingService: LoadingService ) { }
 
   ngOnInit(): void {
-    this.isLoading.next(false);
+    this.loadingService.isLoading.next(false);
   }
 
   onLoginSubmit() {
@@ -51,28 +52,23 @@ export class AuthComponent implements OnInit {
       })
       return;
     }
+    this.loadingService.isLoading.next(true);
 
     let user: LoginUser = {
-      login: (document.getElementById('login') as HTMLInputElement).value,
+      email: (document.getElementById('email') as HTMLInputElement).value,
       password: (document.getElementById('password') as HTMLInputElement).value
     };
     this.authService.login(user).subscribe(res => {
       this.router.navigateByUrl('/')
+      this.loadingService.isLoading.next(false);
     });
   }
 
   onRegistrationSubmit() {
-    if (this.registrationForm.invalid) {
-      this.nzNotificationService.error('Ошибка', 'Форма заполнена неверно');
-      Object.values(this.registrationForm.controls).forEach(control => {
-        if (control.invalid) {
-          control.markAsTouched();
-          control.updateValueAndValidity({onlySelf: true});
-        }
-      })
-      return;
-    }
-    if(this.registrationForm.controls.password != this.registrationForm.controls.passwordCheck) {
+    
+    if(this.registrationForm.controls.password.value != this.registrationForm.controls.passwordCheck.value && this.registrationForm.controls.password.value != null && this.registrationForm.controls.passwordCheck.value != null) {
+      console.log(this.registrationForm.controls.passwordCheck.value)
+      console.log(this.registrationForm.controls.password.value);
       this.nzNotificationService.error('Ошибка', 'Пароли не совпадают');
       this.registrationForm.controls.password.reset();
       this.registrationForm.controls.password.markAsTouched();
@@ -85,14 +81,27 @@ export class AuthComponent implements OnInit {
       
       return;
     }
+    if (this.registrationForm.invalid) {
+      this.nzNotificationService.error('Ошибка', 'Форма заполнена неверно');
+      Object.values(this.registrationForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsTouched();
+          control.updateValueAndValidity({onlySelf: true});
+        }
+      })
+      return;
+    }
+    this.loadingService.isLoading.next(true);
 
     let user: RegistrationUser = {
-      login: (document.getElementById('login') as HTMLInputElement).value,
+      name: (document.getElementById('name') as HTMLInputElement).value,
       email: (document.getElementById('email') as HTMLInputElement).value,
       password: (document.getElementById('password') as HTMLInputElement).value
     };
     this.authService.register(user).subscribe(res => {
-      this.router.navigateByUrl('/auth')
+      this.nzNotificationService.success("Успешно", "Пользователь зарегистрирован");
+      this.isLogin.next(true);
+      this.loadingService.isLoading.next(false);
     });
   }
 
